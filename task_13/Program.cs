6bd0bbdf41491f0fd7e1a657ed91086e965956cf
Alexej Ananin <vicont100@yandex.ru>
2023-10-16 00:28:57 +0300

    initial commit

---
+++ task_13/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 326791123 -> 6
+
+System.Console.Write("Введите целое число: ");
+int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+if (number < 100)
+    { System.Console.WriteLine(number);
+        System.Console.WriteLine("Третьей цифры нет");
+    }
+    else
+        {
+           while (number > 1000)
+           {
+                number = number / 10;
+           }
+           System.Console.WriteLine($"Третья цифра числа:  {number % 10}");
+        }
---
+++ task_15/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.Write("Введите день недели: ");
+int weekDay = Convert.ToInt32(Console.ReadLine());
+
+if (weekDay > 7 || weekDay < 1)
+{
+    System.Console.WriteLine("Такой день недели не существует");
+}
+else if (weekDay == 6 || weekDay == 7)
+{
+    System.Console.WriteLine("да");
+}
+else {
+    System.Console.WriteLine("нет");
+}
